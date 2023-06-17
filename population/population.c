cad920ed51ea52af485e51070739a8e229f69e67
evieran <124214396+evieran@users.noreply.github.com>
2023-06-17 23:02:05 +0000

    Sat, Jun 17, 2023, 11:02 PM +00:00

--- population/population.c
+++ population/population.c
@@ -3,37 +3,28 @@
 
 int main(void)
 {
-    int start;
+    int start, end, years = 0;
+
     printf("Enter the starting populatipn size (minimum 9): ");
-    scanf("%d", &start);
+    scanf("%i", &start);
 
     while (start < 9) {
         printf("Starting population size must be at least 9. Please enter again: ");
-        scanf("%d", &start_population);
+        scanf("%i", &start_population);
     }
 
     printf("Enter the ending population size (must be greater than or equal to starting point size): ");
-    scanf("%d", &end);
+    scanf("%i", &end);
 
     while (end < start) {
         printf("Ending population size must be greater than or equal to starting population size. Please enter again: ");
-        scanf("%d", &end);
+        scanf("%i", &end);
     }
 
     while (start < end) {
-        
-    }
-
-    int years = 0;
-
-    while (start < end)
-    {
         start = start + (start / 3) - (start / 4);
         years++;
     }
 
-    // TODO: Calculate number of years until we reach threshold
-
-    // TODO: Print number of years
     print("Years: %i\n", years);
 }
