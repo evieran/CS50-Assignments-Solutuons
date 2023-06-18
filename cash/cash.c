9e2c112b31242854631c3eb9deb83e65699ed806
evieran <124214396+evieran@users.noreply.github.com>
2023-06-18 11:00:11 +0000

    Sun, Jun 18, 2023, 2:00 PM +03:00

--- cash/cash.c
+++ cash/cash.c
@@ -38,29 +38,35 @@ int main(void)
 int get_cents(void)
 {
     // TODO
-    return 0;
+    int cents;
+    do
+    {
+        cents = get_int("Enter the number of cents: ");
+    }
+    while (cents < 0);
+    return cents;
 }
 
 int calculate_quarters(int cents)
 {
     // TODO
-    return 0;
+    return cents / 25;
 }
 
 int calculate_dimes(int cents)
 {
     // TODO
-    return 0;
+    return cents / 10;
 }
 
 int calculate_nickels(int cents)
 {
     // TODO
-    return 0;
+    return cents / 5;
 }
 
 int calculate_pennies(int cents)
 {
     // TODO
-    return 0;
+    return cents;
 }
