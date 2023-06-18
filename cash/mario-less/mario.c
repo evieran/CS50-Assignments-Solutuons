29ca494d3891fd4f0ab566e987e5b9487309b297
evieran <124214396+evieran@users.noreply.github.com>
2023-06-18 11:36:18 +0000

    Sun, Jun 18, 2023, 2:36 PM +03:00

--- cash/mario-less/mario.c
+++ cash/mario-less/mario.c
@@ -1,7 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // Variable to stop the height of the pyramid
+    ints height;
+
+    // Prompt the user for a positive integer between 1 and 8, inclusive
+    do
+    {
+        height = get_int("Enter the height of the pyramid (1-8): ");
+    }
+    while (height < 1 || height > 8);
+
+    // Print the value of height to confirm it's stored successfully
+    printf("Stored height: %d\n", height);
+
+    return 0;
 }
