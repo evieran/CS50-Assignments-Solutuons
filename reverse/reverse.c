9e0b93966dcfcf5294b32416dfd68c2b00e9d4b0
evieran <124214396+evieran@users.noreply.github.com>
2023-06-19 17:37:08 +0000

    Mon, Jun 19, 2023, 8:37 PM +03:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "wav.h"
 
@@ -10,13 +11,25 @@ int get_block_size(WAVHEADER header);
 int main(int argc, char *argv[])
 {
     // Ensure proper usage
-    // TODO #1
+    if (argc != 3) {
+        fprintf(stderr, "Usage: ./reverse input.wav output.wav\n");
+        return 1;
+    }
 
-    // Open input file for reading
-    // TODO #2
+    // TODO #2: Open input file for reading
+    FILE *input == fopen(argv[1], "r");
+    if (input == NULL) {
+        fprintf(stderr, "Could not open file %s.\n", argv[1]);
+        return 1;
+    }
 
-    // Read header
-    // TODO #3
+    // TODO #3: Read header
+    WAVEHEADER header;
+    if (fread(&header, sizeof(WAVEHEADER), 1, input) != 1) {
+        fprintf(stderr, "Could not read header from %s. \n", argv[1]);
+        fclose(input);
+        return 1;
+    }
 
     // Use check_format to ensure WAV format
     // TODO #4
