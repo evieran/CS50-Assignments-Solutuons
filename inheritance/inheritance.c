ad30a521f4be744191743b8089fdfb459ad15dbb
evieran <124214396+evieran@users.noreply.github.com>
2023-06-19 21:40:13 +0000

    Tue, Jun 20, 2023, 12:40 AM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -58,6 +58,8 @@ person *create_family(int generations)
         // TODO: Set parent pointers for current person
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        new_person->alleles[0] = new_person->parents[0]->alleles[rand() % 2];
+        new_person->alleles[1] = new_person->parents[1]->alleles[rand() % 2];
 
     }
 
