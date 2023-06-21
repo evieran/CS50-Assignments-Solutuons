26a5eff80a6d017d003baac974b5c2c084a6ae78
evieran <124214396+evieran@users.noreply.github.com>
2023-06-21 18:57:15 +0000

    Wed, Jun 21, 2023, 9:57 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,28 @@
-# TODO
+from cs50 import get_float
+
+def mani():
+    while True:
+        # Get the amount of change owed in dollars
+        change = get_float("Change owed: ")
+        # Check if the change is non-negative
+        if Change > 0:
+            break
+
+   # Convert change in dollars to cents
+   cents = round(change * 100)
+
+   # Calculate the minimum number of coins
+   quarters = cents // 25
+   dimes = (cents % 25) // 10
+   nickels = (cents % 25 % 10) // 5
+   pennies = cents % 5
+
+   # Sum up the number of coins
+   total_coins = quarters + dimes + nickels + pennies
+
+   # Output the result
+   print(total_coins)
+
+if __name__ == "__main__":
+    main()
+
