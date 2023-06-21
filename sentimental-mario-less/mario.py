1eda376b62eb8381be22db4df91dda11ebda9c54
evieran <124214396+evieran@users.noreply.github.com>
2023-06-21 18:46:03 +0000

    Wed, Jun 21, 2023, 9:46 PM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,26 @@
-# TODO
+def get_height():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if 1 <= height <= 8:
+                return height
+            except ValueError:
+                pass
+
+def main():
+    # Get the height of the pyramid
+    height = get_height()
+
+    # Build the pyramid
+    for i in range(1, height +1):
+        # Print space
+        for j in range(height - i):
+            print(" ", end="")
+        # Print hashes
+        for k in range(i):
+            print("#", end="")
+        # New line
+        print()
+
+if __name__ == "__main__":
+    main()
