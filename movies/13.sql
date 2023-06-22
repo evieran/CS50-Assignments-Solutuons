df5feffa1cd9704e8b0d0fea4949713ef3803e1c
evieran <124214396+evieran@users.noreply.github.com>
2023-06-22 09:57:25 +0000

    Thu, Jun 22, 2023, 12:57 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -3,5 +3,5 @@ FROM stars
 JOIN people AS kevin_bacon ON stars.person_id = kevin_bacon.id
 JOIN stars AS co_stars_stars ON stars.movie_id = co_stars_stars.movie_id
 JOIN people AS co_stars ON co_stars_stars.person_id = co_stars.id
-WHERE kevin_bacon.name = 'Kevin Bacon' AND kevin_bacon.birth_year = 1958
+WHERE kevin_bacon.name = 'Kevin Bacon' AND kevin_bacon.birth = 1958
 AND co_stars.name != 'Kevin Bacon';
