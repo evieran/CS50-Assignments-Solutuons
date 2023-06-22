8c2ae9c405efdc7bb6d87a85c3d5bfb1c5e4ef13
evieran <124214396+evieran@users.noreply.github.com>
2023-06-22 10:36:02 +0000

    Thu, Jun 22, 2023, 1:36 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -41,6 +41,20 @@ FROM atm_transactions AS atm, phone_calls AS pc
 WHERE atm.year = pc.year AND atm.month = pc.month AND atm.day = pc.day
 AND (atm.year = 2021 AND atm.month = 7 AND atm.day >= 28);
 
+-- Identify potential suspects:
+SELECT *
+FROM atm_transactions
+WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Humphrey Street';
+
+SELECT *
+FROM phone_calls
+WHERE year = 2021 AND month = 7 AND day = 28 AND (caller IN (list_of_account_numbers) OR receiver IN (list_of_account_numbers));
+
+-- Interview Witnesses:
+SELECT name, transcript
+FROM interviews
+WHERE year = 2021 AND month = 7 AND day >= 28;
+
 -- If you have suspects, you can narrow down searches to specific individuals.
 SELECT *
 FROM interviews
