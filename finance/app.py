396d7025844cedf60816f5770002ab6dcc8222af
evieran <124214396+evieran@users.noreply.github.com>
2023-06-22 22:50:58 +0000

    Fri, Jun 23, 2023, 1:50 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -266,3 +266,6 @@ def sell():
         stocks = db.execute("SELECT DISTINCT symbol FROM transactions WHERE user_id = ?", session["user_id"])
 
         return render_template("sell.html", stocks=stocks)
+
+if __name__ == "__main__": 
+    app.run(debug=True)
