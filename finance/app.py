e92d4eb70cd6e873b2456e8ca15e496140b618f3
evieran <124214396+evieran@users.noreply.github.com>
2023-06-22 22:55:21 +0000

    Fri, Jun 23, 2023, 1:55 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -1,6 +1,8 @@
 import os
 import logging
 
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
