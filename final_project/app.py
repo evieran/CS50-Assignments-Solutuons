3b165e08f288b63eb2dd957573c030d439bad074
evieran <124214396+evieran@users.noreply.github.com>
2023-07-06 17:20:19 +0000

    Thu, Jul 6, 2023, 8:20 PM +03:00

--- final_project/app.py
+++ final_project/app.py
@@ -10,6 +10,9 @@ from helpers import login_required
 
 app = Flask(__name__)
 
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///yourdatabase.db")
+
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
