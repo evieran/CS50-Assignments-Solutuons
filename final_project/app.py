79bec9e67a9a0f61a3e39c64483a6f0a947df9fc
evieran <124214396+evieran@users.noreply.github.com>
2023-07-06 17:31:22 +0000

    Thu, Jul 6, 2023, 8:31 PM +03:00

--- final_project/app.py
+++ final_project/app.py
@@ -72,6 +72,53 @@ def register():
     else:
         return render_template("register.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("apology.html", message="must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("apology.html", message="must provide password")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("apology.html", message="invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/login")
+
+
 cognitive_distortions = {
     "black_and_white": {
         "explanation": "Seeing things in only two categories (good or bad, success or failure) without acknowledging any spectrum in between.",
