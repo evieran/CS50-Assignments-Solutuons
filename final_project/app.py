2a1c42ccaf34770a7fb5a4e0a3bb846a76d0fae3
evieran <124214396+evieran@users.noreply.github.com>
2023-07-06 17:14:20 +0000

    Thu, Jul 6, 2023, 8:14 PM +03:00

--- final_project/app.py
+++ final_project/app.py
@@ -10,6 +10,22 @@ from helpers import apology, login_required
 
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 cognitive_distortions = {
     "black_and_white": {
         "explanation": "Seeing things in only two categories (good or bad, success or failure) without acknowledging any spectrum in between.",
