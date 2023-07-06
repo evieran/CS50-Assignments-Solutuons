61a015dd7ecdce33e23f4371cfe754621bd332f6
evieran <124214396+evieran@users.noreply.github.com>
2023-07-06 17:13:03 +0000

    Thu, Jul 6, 2023, 8:13 PM +03:00

--- final_project/app.py
+++ final_project/app.py
@@ -1,19 +1,12 @@
 import datetime
 import random
-from flask import Flask, render_template, request, redirect, session, url_for
-from flask_session import Session
-from cs50 import SQL
-from werkzeug.security import generate_password_hash, check_password_hash
-from helpers import login_required
-
-
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 
 app = Flask(__name__)
 
