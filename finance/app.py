b3234aa38dcff0cdfdc8a373794555ef563fcc8f
evieran <124214396+evieran@users.noreply.github.com>
2023-07-06 10:14:55 +0000

    Thu, Jul 6, 2023, 1:14 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -98,8 +98,7 @@ def index():
         total_value += stock["total_value"]
         grand_total += stock["total_value"]
 
-
-return render_template("index.html", stocks=stocks, cash=cash, total_value=total_value, grand_total=grand_total)
+    return render_template("index.html", stocks=stocks, cash=cash, total_value=total_value, grand_total=grand_total)
 
 
 @app.route("/buy", methods=["GET", "POST"])
