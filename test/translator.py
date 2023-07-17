0d6de09d6aa301c7fac9e4c1653132fd48aeb439
evieran <124214396+evieran@users.noreply.github.com>
2023-07-17 13:08:20 +0000

    Mon, Jul 17, 2023, 4:08 PM +03:00

--- test/translator.py
+++ test/translator.py
@@ -3,11 +3,29 @@ import os
 from deep_translator import MyMemoryTranslator
 
 def english_to_french(text):
+    """
+    Translate English text to French.
+
+    Parameters:
+    text (str): English text to translate
+
+    Returns:
+    str: Translated French text
+    """
     translator = MyMemoryTranslator(source='en', target='fr')
     translation = translator.translate(text)
     return translation
 
 def french_to_english(text):
+    """
+    Translate French text to English.
+
+    Parameters:
+    text (str): French text to translate
+
+    Returns:
+    str: Translated English text
+    """
     translator = MyMemoryTranslator(source='fr', target='en')
     translation = translator.translate(text)
     return translation
