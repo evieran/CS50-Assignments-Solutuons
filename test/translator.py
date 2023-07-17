c634f07030eb7a47206290b42d9d057aa7a31b23
evieran <124214396+evieran@users.noreply.github.com>
2023-07-17 13:19:13 +0000

    Mon, Jul 17, 2023, 4:19 PM +03:00

--- test/translator.py
+++ test/translator.py
@@ -5,15 +5,13 @@ This module provides translation functions for English and French.
 from deep_translator import MyMemoryTranslator
 
 def english_to_french(text):
-    """
-    Translate English text to French.
-
-    Parameters:
-    text (str): English text to translate
-
-    Returns:
-    str: Translated French text
-    """
+    try:
+        translator = MyMemoryTranslator(source='english', target='french')
+        return translator.translate(text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
     translator = MyMemoryTranslator(source='en', target='fr')
     translation = translator.translate(text)
     return translation
