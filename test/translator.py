9001d523a9de0847c6ec53d7f71a3bad79921cf6
evieran <124214396+evieran@users.noreply.github.com>
2023-07-17 13:23:37 +0000

    Mon, Jul 17, 2023, 4:23 PM +03:00

--- test/translator.py
+++ test/translator.py
@@ -17,26 +17,9 @@ def english_to_french(text):
     try:
         translator = MyMemoryTranslator(source='english', target='french')
         return translator.translate(text)
-    except Exception as err:
+    except Exception as e:
         print(f"An error occurred: {err}")
         return None
-Likewise, you would update the french_to_english function:
-
-python
-Copy code
-def french_to_english(text):
-    try:
-        translator = MyMemoryTranslator(source='french', target
-
-
-Stop generating
-Send a message
-
-··
-ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 24 Version
-Check Python Code Standards
-
-Be careful about sharing sensitiv
 
 def french_to_english(text):
     """
@@ -52,5 +35,5 @@ def french_to_english(text):
         translator = MyMemoryTranslator(source='french', target='english')
         return translator.translate(text)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: {err}")
         return None
