6c569482ba7266cfbc4c631511bfee5cfc3a9615
evieran <124214396+evieran@users.noreply.github.com>
2023-07-17 13:05:11 +0000

    Mon, Jul 17, 2023, 4:05 PM +03:00

---
+++ test/translator.py
@@ -0,0 +1,11 @@
+from deep_translator import MyMemoryTranslator
+
+def english_to_french(text):
+    translator = MyMemoryTranslator(source='en', target='fr')
+    translation = translator.translate(text)
+    return translation
+
+def french_to_english(text):
+    translator = MyMemoryTranslator(source='fr', target='en')
+    translation = translator.translate(text)
+    return translation
